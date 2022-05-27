88331457e4dcaaaf96e699a4eb56ec6f3e4d6408
Trillium <spiteless@gmail.com>
2022-05-27 09:25:05 -0700

    Add ContextProvider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import { ContextProvider } from "../context/AppContext";
 
 import theme from "../themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <ContextProvider>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ContextProvider>
     </ThemeProvider>
   );
 }
