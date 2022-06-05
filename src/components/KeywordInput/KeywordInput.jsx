bf82ae0991e00b0db1e9b74c5857e31180d98f49
Trillium Smith <spiteless@gmail.com>
2022-06-04 22:44:13 -0700

    feat: Use Autocomplete, Chips to for keywords

--- src/components/KeywordInput/KeywordInput.jsx
+++ src/components/KeywordInput/KeywordInput.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import { TextField, Button, ButtonGroup } from "@mui/material";
+
+import Autocomplete from "@mui/material/Autocomplete";
+import Chip from "@mui/material/Chip";
+import Checkbox from "@mui/material/Checkbox";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
 import { useAppState } from "@context/AppContext";
 
 export default function KeywordInput(props) {
-   const router = useRouter();
   const appState = useAppState();
   const {
     context,
@@ -18,17 +22,6 @@ export default function KeywordInput(props) {
     setDisabled,
     disabled,
   } = useAppState();
-
-  const copyToClipboard = (e) => {
-    let text = "";
-    text = keywordsProcessed
-      .filter((val) => val[1] === 0)
-      .map((item) => `${item[0]}`)
-      .join("\n");
-    text = "%% MISSING WORDS\n" + text + "\n%%";
-    navigator.clipboard.writeText(text);
-    e.target.focus();
-  };
 
   const handleKeywordsChange = (e) => {
     const newState = { ...keywords };
@@ -43,63 +36,111 @@ export default function KeywordInput(props) {
 
   return (
     <>
-      {/* <pre>{keywordsProcessed}</pre> */}
-      <TextField
-        name="keywordField"
-        value={
-          disabled
-            ? keywordsProcessed
-                .map((item) => `${item[1]} ${item[0]}`)
-                .join("\n")
-            : keywords.text
-        }
-        onChange={handleKeywordsChange}
-        variant="outlined"
-        color="secondary"
-        label={
-          keywordsProcessed.length > 0
-            ? `Unused Keywords: ${
-                keywordsProcessed.filter((item) => item[1] === 0).length
-              }/${keywordsProcessed.length}`
-            : "Enter keywords to begin - New line for each term."
-        }
-        onFocus={() => {
-          setDisabled(false);
-        }}
-        onBlur={() => {
-          setDisabled(true);
-        }}
-        fullWidth
-        multiline
-        minRows={2}
-      />
-      <ButtonGroup
-        fullWidth
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexGrow: 1,
-          whiteSpace: "nowrap",
-        }}
-      >
-        <Button
-          style={{ marginTop: 5, minWidth: 140 }}
-          variant="outlined"
-          color="primary"
-          onClick={copyToClipboard}
-        >
-          Copy All
-        </Button>
-        <Button
-          style={{ marginTop: 5, minWidth: 140 }}
-          variant="outlined"
-          color="primary"
-          onClick={copyToClipboard}
-        >
-          Copy Missing
-        </Button>
-      </ButtonGroup>
+      <Tags />
+      {/* {console.log(appState)} */}
+      {/* <Buttons /> */}
     </>
   );
 }
+
+const Buttons = () => {
+  const appState = useAppState();
+  const { searchFuncs } = appState;
+  const { copyToClipboard } =
+    searchFuncs.functionNames["copyMissingToClipboard()"];
+
+  return (
+    <ButtonGroup
+      fullWidth
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexGrow: 1,
+        whiteSpace: "nowrap",
+      }}
+    >
+      <Button
+        style={{ marginTop: 5, minWidth: 140 }}
+        variant="outlined"
+        color="primary"
+        onClick={copyToClipboard}
+      >
+        Copy All
+      </Button>
+      <Button
+        style={{ marginTop: 5, minWidth: 140 }}
+        variant="outlined"
+        color="primary"
+        onClick={copyToClipboard}
+      >
+        Copy Missing
+      </Button>
+    </ButtonGroup>
+  );
+};
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+const Tags = () => {
+  const appState = useAppState();
+  const {
+    context,
+    setContext,
+    keywords,
+    setKeywords,
+    keywordsProcessed,
+    setKeywordsProcessed,
+    setRouter,
+    setDisabled,
+    disabled,
+  } = useAppState();
+
+  const [value, setValue] = useState(keywordsProcessed);
+  const [inputValue, setInputValue] = useState("");
+
+  // const handleChange = () => {
+  //   setValue(keywordsProcessed.filter(phrase => phrase.count === 0));
+  // };
+
+  return (
+    <Autocomplete
+      multiple
+      id="tags-filled"
+      options={keywordsProcessed}
+      getOptionLabel={(option) => option.name}
+      value={keywordsProcessed}
+      renderOption={(props, option, { selected }) => (
+        <li {...props} key={option.name}>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            checked={option.count > 0}
+          />
+          {props.key}
+        </li>
+      )}
+      renderTags={(tagValue, getTagProps) =>
+        tagValue.map((option, index) => (
+          <Chip
+            label={`${option.name}: ${option.count}`}
+            {...getTagProps({ index })}
+            color={option.count > 0 ? "success" : "default"}
+          />
+        ))
+      }
+      freeSolo
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          label={`${keywordsProcessed.filter((k) => k.count > 0).length}/${
+            keywordsProcessed.length
+          } used`}
+        />
+      )}
+    />
+  );
+};
