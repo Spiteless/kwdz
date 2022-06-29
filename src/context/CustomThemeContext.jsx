0386d75eaa6d70b262877de85dd04c7b5df6ff64
Trillium Smith <spiteless@gmail.com>
2022-06-28 17:43:15 -0700

    chore: Clear out text artifact from bad merge

--- src/context/CustomThemeContext.jsx
+++ src/context/CustomThemeContext.jsx
@@ -23,10 +23,7 @@ export default function ThemeContext({ children }) {
       setNum(num + 1);
     }
     if (nextTheme) setActiveTheme(nextTheme);
-<<<<<<< HEAD
 
-=======
->>>>>>> 0c428c6 (fix: Remove console.log, update yarn.lock)
   };
 
   const exports = {
